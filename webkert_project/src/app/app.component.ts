930add8fab24cd5390486f61e9c35ac990efc935
Dominik <h145108@stud.u-szeged.hu>
2025-04-10 15:01:22 +0200

    add: Login component; test: HTML

--- webkert_project/src/app/app.component.ts
+++ webkert_project/src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { CompletedComponent } from './pages/completed/completed.component';
+import { LoginComponent } from './pages/login/login.component';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent],
+  imports: [
+    RouterOutlet, HomeComponent, ProfileComponent, CompletedComponent, MenuComponent, LoginComponent, NgIf
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
